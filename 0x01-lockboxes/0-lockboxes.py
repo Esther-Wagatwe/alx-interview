d9be2548c6805bb37c763f1bdc76424af94d1324
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-08-13 12:12:23 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+
+    unlocked = [False] * n
+
+    unlocked[0] = True
+
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    return all(unlocked)
