8c9bbdce2c94c8418bb1dabc9b88f0237fc47e4b
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-10-02 11:18:48 +0300

    Debugging

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,14 @@ def makeChange(coins, total):
     using the least amount of coins."""
     if total <= 0:
         return 0
-
+    coins.sort(reverse=True)
     arr = [float('inf')] * (total + 1)
 
     arr[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
+    for coin in coins:
+        for i in range(coin, total + 1):
+            if i - coin >= 0:
                 arr[i] = min(arr[i], arr[i - coin] + 1)
 
     return arr[total] if arr[total] != float('inf') else -1
